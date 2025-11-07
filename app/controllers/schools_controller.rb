d42963265782462c90c9d9a05fbb9e4ce7b138f2
mkaur124 <imuskansandhu04@gmail.com>
2025-11-06 21:57:08 -0600

    chore(pagination): kaminari installed and controllers updated for pagination

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,6 +1,7 @@
 class SchoolsController < ApplicationController
   def index
     @q = params[:q]
+    per_page = (params[:per_page] || 20).to_i   # Allow user to select number of records per page
     scope = School.all
 
     # Search by name or city (case-insensitive)
@@ -10,16 +11,17 @@ class SchoolsController < ApplicationController
       scope = scope.where("name #{like_operator} ? OR city #{like_operator} ?", "%#{@q}%", "%#{@q}%")
     end
 
-    # Optional filtering by city (if filter dropdown is added later)
+    # Optional filtering by city (if you add a dropdown later)
     scope = scope.where(city: params[:city]) if params[:city].present?
 
     # Pagination with Kaminari
-    @schools = scope.order(:name).page(params[:page]).per(20)
+    @schools = scope.order(:name).page(params[:page]).per(per_page)
   end
 
   def show
     @school = School.find(params[:id])
     @teachers = @school.teachers.order(:last_name)
-    @students = @school.students.order(:last_name) if @school.respond_to?(:students)
+    # Students associated through teachers
+    @students = Student.where(teacher_id: @school.teachers.pluck(:id)).order(:last_name)
   end
 end
